bba6daa9f70ece3568faae235624bec65cace1bd
cherrydub <cherrydub@gmail.com>
2023-05-24 19:39:45 +0100

    more tinkering with .env, possible issues with vercel?

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import FaceRecognition from "./components/FaceRecognition";
 import Rank from "./components/Rank";
 import ParticlesBg from "particles-bg";
 import SignIn from "./components/Login/SignIn";
-// const apiKey = import.meta.env.VITE_API_KEY;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 function App() {
   const [input, setInput] = useState("");
@@ -79,7 +79,7 @@ function App() {
       method: "POST",
       headers: {
         Accept: "application/json",
-        Authorization: "Key " + import.meta.env.VITE_API_KEY,
+        Authorization: "Key " + apiKey,
       },
       body: raw,
     };
@@ -119,7 +119,6 @@ function App() {
       <ImageLinkForm setInput={setInput} />
 
       <FaceRecognition input={input} imgData={imgData} box={box} />
-      {/* {console.log("real box:", box)} */}
     </>
   );
 }
