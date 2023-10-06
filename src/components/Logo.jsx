0b26f4223db90eee962fcf943dc1f68af0867fc6
cherrydub <cherrydub@gmail.com>
2023-10-06 11:10:35 +0100

    finally fixed logo page correctly

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -37,54 +37,56 @@ export default function Logo({ route, onRouteChange }) {
         <div className="bg-white opacity-90 text-center">
           <div className="text-center flex items-center justify-center align-center">
             <img src={brain} alt="brain logo" width="100px" />
-            <p>
-              For Demo purposes feel free to{" "}
-              <span
-                onClick={() => onRouteChange("register")}
-                className="cursor-pointer hover:text-violet-700 font-bold"
-              >
-                register
-              </span>{" "}
-              or use a{" "}
-              <span
-                onClick={() => onRouteChange("signin")}
-                className="cursor-pointer hover:text-violet-700 font-bold"
-              >
-                login
-              </span>
-              :
-            </p>
-            <div className="flex items-center">
-              <img
-                src="https://www.svgrepo.com/show/479446/email-14.svg"
-                alt=""
-                title="copy email"
-                width="20px"
-                className="mr-2"
-              />
-              <button
-                className="clipboard-btn font-bold text-violet-500 hover:underline"
-                onClick={() => handleCopyToClipboard("test@gmail.com")}
-                title="copy"
-              >
-                test@gmail.com
-              </button>
-            </div>
-            <div className="flex items-center mt-2">
-              <img
-                src="https://www.svgrepo.com/show/274446/key-password.svg"
-                alt=""
-                title="copy password"
-                width="20px"
-                className="mr-2"
-              />
-              <button
-                className="clipboard-btn font-bold text-violet-500 hover:underline"
-                onClick={() => handleCopyToClipboard("test")}
-                title="copy"
-              >
-                test
-              </button>
+            <div>
+              <p>
+                For Demo purposes feel free to{" "}
+                <span
+                  onClick={() => onRouteChange("register")}
+                  className="cursor-pointer hover:text-violet-700 font-bold"
+                >
+                  register
+                </span>{" "}
+                or use a{" "}
+                <span
+                  onClick={() => onRouteChange("signin")}
+                  className="cursor-pointer hover:text-violet-700 font-bold"
+                >
+                  login
+                </span>
+                :
+              </p>
+              <div className="flex items-center">
+                <img
+                  src="https://www.svgrepo.com/show/479446/email-14.svg"
+                  alt=""
+                  title="copy email"
+                  width="20px"
+                  className="mr-2"
+                />
+                <button
+                  className="clipboard-btn font-bold text-violet-500 hover:underline"
+                  onClick={() => handleCopyToClipboard("test@gmail.com")}
+                  title="copy"
+                >
+                  test@gmail.com
+                </button>
+              </div>
+              <div className="flex items-center mt-2">
+                <img
+                  src="https://www.svgrepo.com/show/274446/key-password.svg"
+                  alt=""
+                  title="copy password"
+                  width="20px"
+                  className="mr-2"
+                />
+                <button
+                  className="clipboard-btn font-bold text-violet-500 hover:underline"
+                  onClick={() => handleCopyToClipboard("test")}
+                  title="copy"
+                >
+                  test
+                </button>
+              </div>
             </div>
           </div>
         </div>
